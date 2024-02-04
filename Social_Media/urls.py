b6e55fdd57a4ea918036dc9c74c20d8182a44402
AliKazempour <Alikazempoor83@gmail.com>
2024-02-04 19:37:17 +0330

    ✨ feat : adding Comment Views

--- Social_Media/urls.py
+++ Social_Media/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ListCreatePostView,RetrieveUpdateDestroyPostView
+from .views import ListCreatePostView, RetrieveUpdateDestroyPostView, ListCreateCommentView, RetrieveUpdateDestroyCommentView
 urlpatterns = [
     path('posts/', ListCreatePostView.as_view(), name='posts'),
-    path('posts/<int:pk>/', RetrieveUpdateDestroyPostView.as_view(), name='post-details'),
+    path('posts/<int:pk>/', RetrieveUpdateDestroyPostView.as_view(),
+         name='post-details'),
+    path('posts/<int:post_id>/comments/',
+         ListCreateCommentView.as_view(), name='comments'),
+    path('posts/<int:post_id>/comments/<int:pk>/',
+         RetrieveUpdateDestroyCommentView.as_view(), name='comment-details'),
 
 ]
--- Social_Media/views.py
+++ Social_Media/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import User, Post, Comment, Like, Follower, SavedPost
-from .serializers import PostSerializer, PostRetrieveUpdateDestroySerializer
+from .serializers import PostSerializer, PostRetrieveUpdateDestroySerializer, CommentSerializer, CommentRetrieveUpdateDestroySerializer
 from rest_framework import generics
 
 
@@ -16,9 +16,36 @@ class ListCreatePostView(generics.ListCreateAPIView):
 
 class RetrieveUpdateDestroyPostView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostRetrieveUpdateDestroySerializer
+
     def get_object(self):
         """
         Retrieve and return a specific Post instance based on the pk parameter.
         """
         post_id = self.kwargs.get('pk', None)
         return Post.objects.get(id=post_id)
+
+
+class ListCreateCommentView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        """
+        Return the queryset for all Comment objects.
+        """
+        post_id = self.kwargs.get('post_id', None)
+        return Comment.objects.filter(post=post_id)
+
+
+class RetrieveUpdateDestroyCommentView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CommentRetrieveUpdateDestroySerializer
+
+    def get_object(self):
+        """
+        Retrieve a comment object from the database based on the provided post_id and comment_id.
+
+        Returns:
+            Comment: The retrieved Comment object.
+        """
+        post_id = self.kwargs.get('post_id', None)
+        comment_id = self.kwargs.get('pk', None)
+        return Comment.objects.get(id=comment_id, post=post_id)
