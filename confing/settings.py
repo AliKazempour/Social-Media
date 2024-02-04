20e5f3f988c4138db40f28d6c09bb93c6dd595e9
AliKazempour <Alikazempoor83@gmail.com>
2024-02-04 13:04:26 +0330

    ✅ updating settings.py

--- confing/settings.py
+++ confing/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'Social_Media',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PHONENUMBER_DEFAULT_REGION = 'US'
+AUTH_USER_MODEL = 'Social_Media.User'
