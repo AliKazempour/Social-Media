b07e89e42e158115c2033cad543f306ee35f405b
AliKazempour <Alikazempoor83@gmail.com>
2024-02-04 18:38:41 +0330

    ✨ feat : adding post views

--- Social_Media/views.py
+++ Social_Media/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import User, Post, Comment, Like, Follower, SavedPost
+from .serializers import PostSerializer, PostRetrieveUpdateDestroySerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ListCreatePostView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        """
+        Return the queryset for all Post objects.
+        """
+        return Post.objects.all()
+
+
+class RetrieveUpdateDestroyPostView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostRetrieveUpdateDestroySerializer
+    def get_object(self):
+        """
+        Retrieve and return a specific Post instance based on the pk parameter.
+        """
+        post_id = self.kwargs.get('pk', None)
+        return Post.objects.get(id=post_id)
