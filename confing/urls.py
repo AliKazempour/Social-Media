bc1b02319ad284cf53f1804e6145202e995208b5
AliKazempour <Alikazempoor83@gmail.com>
2024-02-09 11:14:54 +0330

    ✨ feat : adding api-auth from rest_framework in confing.urls

--- confing/urls.py
+++ confing/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Social_Media.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
