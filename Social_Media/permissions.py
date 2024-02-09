3ddb7653dee4635d3fbf72c2d756f9775781f2df
AliKazempour <Alikazempoor83@gmail.com>
2024-02-09 13:07:37 +0330

    ✨ feat : adding PostUserEditPermission

--- Social_Media/permissions.py
+++ Social_Media/permissions.py
@@ -1,2 +1,10 @@
 from rest_framework import permissions
 
+
+class PostUserEditPermission(permissions.BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
