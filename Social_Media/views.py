f38d8cbf99ed11f2e9e1a23430606ffddb6bb754
AliKazempour <Alikazempoor83@gmail.com>
2024-03-14 07:50:41 +0330

    ✨ feat : Override the destroy method to decrement the number of comments for the post in RetrieveUpdateDestroyCommentView endpoints

--- Social_Media/views.py
+++ Social_Media/views.py
@@ -93,7 +93,7 @@ class ListCreateCommentView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         """
         Perform the creation of a comment associated with a specific post.
-        
+
         Parameters:
         - serializer: The serializer object to process the data for creating the comment.
         """
@@ -125,6 +125,22 @@ class RetrieveUpdateDestroyCommentView(generics.RetrieveUpdateDestroyAPIView):
         post_id = self.kwargs.get('post_id', None)
         comment_id = self.kwargs.get('pk', None)
         return Comment.objects.get(id=comment_id, post=post_id)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Override the destroy method to decrement the number of comments for the post.
+        """
+        post_id = self.kwargs.get('post_id')
+        post = Post.objects.get(id=post_id)
+        comment_id = self.kwargs.get('pk')
+        comment = Comment.objects.get(id=comment_id, post=post)
+        if comment.post != post:
+            raise ValidationError(
+                'This comment does not belong to the specified post.')
+        post.num_comments -= 1
+        post.save()
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ListCreateSavedPostView(generics.ListCreateAPIView):
